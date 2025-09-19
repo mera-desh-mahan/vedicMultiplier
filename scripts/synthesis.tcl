e0966f426be190a3dd832116f8f3a6c52a4e6986
Rajeev Srivastava <rajeev.srivastava@gmail.com>
2025-09-19 10:22:12 -0400

    updated synthesis script

--- scripts/synthesis.tcl
+++ scripts/synthesis.tcl
@@ -1,15 +1,15 @@
 #TCL written from proton verion-291
-read_verilog /home/ubuntu/vedicMultiplier/src/adder12.v
-read_verilog /home/ubuntu/vedicMultiplier/src/adder16.v
-read_verilog /home/ubuntu/vedicMultiplier/src/adder24.v
-read_verilog /home/ubuntu/vedicMultiplier/src/adder4.v
-read_verilog /home/ubuntu/vedicMultiplier/src/adder6.v
-read_verilog /home/ubuntu/vedicMultiplier/src/adder8.v
-read_verilog /home/ubuntu/vedicMultiplier/src/vedic_8x8.v
-read_verilog /home/ubuntu/vedicMultiplier/src/vedic_4x4.v
-read_verilog /home/ubuntu/vedicMultiplier/src/vedic_2x2.v
-read_verilog /home/ubuntu/vedicMultiplier/src/vedic_16x16.v
-read_verilog /home/ubuntu/vedicMultiplier/src/halfAdder.v
+read_verilog ../src/adder12.v
+read_verilog ../src/adder16.v
+read_verilog ../src/adder24.v
+read_verilog ../src/adder4.v
+read_verilog ../src/adder6.v
+read_verilog ../src/adder8.v
+read_verilog ../src/vedic_8x8.v
+read_verilog ../src/vedic_4x4.v
+read_verilog ../src/vedic_2x2.v
+read_verilog ../src/vedic_16x16.v
+read_verilog ../src/halfAdder.v
 
 hierarchy -check -top vedic_16x16
 
@@ -19,9 +19,9 @@ proc; opt; fsm; opt; memory; opt
 techmap; opt
 
 # mapping flip-flops to mycells.lib
-dfflibmap -liberty /tech/NANGATE/NangateOpenCellLibrary_PDKv1_2_v2008_10_slow_conditional_ecsm.lib
+dfflibmap -liberty /home/rsrivastava/potato/TESTS/library/NangateOpenCellLibrary_PDKv1_2_v2008_10_slow_conditional_ecsm.lib
 # mapping logic to mycells.lib
-abc -liberty /tech/NANGATE/NangateOpenCellLibrary_PDKv1_2_v2008_10_slow_conditional_ecsm.lib
+abc -liberty /home/rsrivastava/potato/TESTS/library/NangateOpenCellLibrary_PDKv1_2_v2008_10_slow_conditional_ecsm.lib
 
 # cleanup
 clean
